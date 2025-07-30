ae70733be9aac36f283ee6c5fc2326d1560952e2
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-07-30 21:09:18 +0900

    feat: add input, button and result tag for even/odd check

---
+++ 04-js-practice/21_even-or-odd/script.js
@@ -0,0 +1,11 @@
+const numInput = document.querySelector("#numInput");
+const checkBtn = document.querySelector("#checkBtn");
+
+checkBtn.addEventListener("click", function () {
+  const value = numInput.value.trim();
+  const number = Number(value);
+   // 여기서부터 조건문으로:
+  // 1. 숫자인지 확인
+  // 2. 짝수/홀수 판별
+  // 3. 결과 출력
+});
