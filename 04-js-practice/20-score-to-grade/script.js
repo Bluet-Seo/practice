3254c46b9dc1eb84a2362b58c31babfbf069ed7a
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-07-30 21:02:44 +0900

    feat: complete score to grade function

--- 04-js-practice/20-score-to-grade/script.js
+++ 04-js-practice/20-score-to-grade/script.js
@@ -6,8 +6,29 @@ checkBtn.addEventListener("click", function () {
   const value = scoreInput.value.trim();
   const score = Number(value);
 
-  // 1. 빈칸이면 경고
-  // 2. 숫자 아니면 경고
-  // 3. 0~100 사이가 아니면 경고
-  // 4. 점수에 따라 A~F 등급 출력
+  if(value===""){
+    alert("점수를 입력해주세요.");
+    return;
+    
+  }else if(isNaN(value)){
+    alert("숫자만 입력가능합니다.");
+    return;
+  }
+
+  if(score>100||score<0){
+    alert("0~100까지의 점수를 입력해주세요")
+     return;
+  }else if(score>90){
+    result.textContent="A등급";
+  }else if(score>80){
+     result.textContent="B등급";
+  }else if(score>70){
+    result.textContent="C등급";
+  }else if(score>60){
+     result.textContent="D등급";
+  }else{
+    result.textContent="F등급";
+  }
+    
+
 });
