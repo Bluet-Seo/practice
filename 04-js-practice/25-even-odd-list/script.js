82cad83b72ec743253c4dc0d59f0f64b708109b8
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-05 23:01:06 +0900

    feat: add even/odd filter UI and button events

---
+++ 04-js-practice/25-even-odd-list/script.js
@@ -0,0 +1,12 @@
+const numInput = document.querySelector("#numInput");
+const evenBtn = document.querySelector("#evenBtn");
+const oddBtn = document.querySelector("#oddBtn");
+const result = document.querySelector("#result");
+
+evenBtn.addEventListener("click", function(){
+
+})
+
+oddBtn.addEventListener("click", function(){
+
+})
