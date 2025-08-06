22b80c07e940c2c6e6779560aa52ce2173cb8c1d
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-06 22:54:45 +0900

    feat: add even/odd filter based on user input

--- 04-js-practice/25-even-odd-list/script.js
+++ 04-js-practice/25-even-odd-list/script.js
@@ -4,9 +4,34 @@ const oddBtn = document.querySelector("#oddBtn");
 const result = document.querySelector("#result");
 
 evenBtn.addEventListener("click", function(){
+    const num2 = Number(numInput.value.trim());
 
-})
+    if(isNaN(num2)){
+        alert("숫자만 입력해주세요");
+        return;
+    }
+    result.innerHTML="";
+
+    for(let i = 1; i<= num2; i++){
+        if(i%2===0){
+             result.innerHTML+=`${i}<br>`
+        }
+    }
+ 
+}) 
 
 oddBtn.addEventListener("click", function(){
+const num1 = Number(numInput.value.trim());
 
+    if(isNaN(num1)){
+        alert("숫자만 입력해주세요");
+        return;
+    }
+    result.innerHTML="";
+
+    for(let i = 1; i<= num1; i++){
+        if(i%2===1){
+             result.innerHTML+=`${i}<br>`
+        }
+    }
 })
