dfaea3d4f0fd4e30f8e3368991f967e1547c8a63
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-07 23:25:11 +0900

    fix: move sum variable outside loop for correct accumulation

---
+++ 04-js-practice/26-sum-calculator/script.js
@@ -0,0 +1,26 @@
+const numInput = document.querySelector("#numInput");
+const sumBtn = document.querySelector("#sumBtn");
+const result = document.querySelector("#result");
+
+sumBtn.addEventListener("click", function(){
+
+    const number = Number(numInput.value.trim());
+
+    if(isNaN(number)){
+        alert("숫자로만 입력해주세요");
+        return;
+    }
+
+    result.innerHTML="";
+    let sum = 0;// 누적용 변수는 for문 바깥에 넣어야 돌릴때마다 0으로 되돌아가지않음
+
+    for(let i = 1; i<=number; i++){
+
+       sum = sum+i;
+
+       result.innerHTML =`1부터 ${number}까지의 합계는 ${sum}입니다.`;
+        
+       
+    }
+})
+
