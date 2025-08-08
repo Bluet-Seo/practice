b5dea98226476099335c22d83b48a5b749bd23df
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-08 22:34:05 +0900

    feat: show cumulative calculation in output

--- 04-js-practice/26-sum-calculator/script.js
+++ 04-js-practice/26-sum-calculator/script.js
@@ -13,14 +13,19 @@ sumBtn.addEventListener("click", function(){
 
     result.innerHTML="";
     let sum = 0;// 누적용 변수는 for문 바깥에 넣어야 돌릴때마다 0으로 되돌아가지않음
+    let expression = "";
 
     for(let i = 1; i<=number; i++){
 
        sum = sum+i;
 
-       result.innerHTML =`1부터 ${number}까지의 합계는 ${sum}입니다.`;
-        
-       
+     if (i === number) {
+  expression += i + " = ";
+} else {
+  expression += i + " + ";
+}
+    result.innerHTML = `${expression}${sum}`;
+    result.innerHTML += `<br>1부터 ${number}까지의 합계는 ${sum}입니다.`;    
     }
-})
+});
 
