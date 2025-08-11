079d3797b07205930392e1c51c5700311828f9c0
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-11 22:36:51 +0900

    feat: add HTML skeleton for multiples filter app

---
+++ 04-js-practice/28-multiples-filter-refactor/script.js
@@ -0,0 +1,8 @@
+const listInput = document.querySelector("#listInput");
+const baseInput = document.querySelector("#baseInput");
+const runBtn = document.querySelector("#runBtn");
+const result = document.querySelector("#result");
+
+runBtn.addEventListener("click", function() {
+  console.log("clicked");
+});
