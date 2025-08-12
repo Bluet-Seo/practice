facc334e8c9f01ee22d3abd1e50ec67545de1da0
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-12 22:40:22 +0900

    feat: Check if input is a number console if 0

--- 04-js-practice/28-multiples-filter-refactor/script.js
+++ 04-js-practice/28-multiples-filter-refactor/script.js
@@ -5,4 +5,21 @@ const result = document.querySelector("#result");
 
 runBtn.addEventListener("click", function() {
   console.log("clicked");
+  const rawList = listInput.value.split(",");
+  const numbers = [];
+  
+  for(let i = 0; i<rawList.length; i++){
+    const value = rawList[i].trim();
+    const num = Number(value);
+
+    if(Number.isFinite(num)){
+      numbers.push(num);
+    }
+  }
+
+  const base =Number(baseInput.value.trim());
+  if(!Number.isFinite(base)||base===0){
+    console.log("유효한 숫자를 입력해주세요.");
+    return;
+  }
 });
