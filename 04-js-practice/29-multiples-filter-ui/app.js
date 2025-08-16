775c0ad16299e4544a09fb9849812aafc91549b7
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-16 20:41:45 +0900

    feat: reset result area when button is clicked

--- 04-js-practice/29-multiples-filter-ui/app.js
+++ 04-js-practice/29-multiples-filter-ui/app.js
@@ -1,1 +1,10 @@
 console.log("안녕하세요?")
+
+const listInput = document.querySelector("#listInput");
+const baseInput = document.querySelector("#baseInput");
+const runBtn = document.querySelector("#runBtn");
+const result = document.querySelector("#result");
+
+runBtn.addEventListener("click", function(){
+    result.textContent="";
+})
