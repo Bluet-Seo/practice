5d8ea4a9a882ff9084302d5237b2ea30a2005759
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-08-21 22:17:36 +0900

    chore: set up multiples-extend skeleton

---
+++ 04-js-practice/30-multiples-filter-extend/app.js
@@ -0,0 +1,8 @@
+const base = document.querySelector("#base");
+const limit = document.querySelector("#limit");
+const checkBtn = document.querySelector("#checkBtn");
+const result = document.querySelector("#result");
+
+checkBtn.addEventListener("click", function(){
+    console.log("안녕하세요? ")
+})
