c3ba408b2138cfe67ba832283cf3888908eaadfa
Bluet-Seo <156332855+Bluet-Seo@users.noreply.github.com>
2025-09-10 17:23:51 +0900

    feat: add random text and color change

--- 04-js-practice/31-dom-practice/script.js
+++ 04-js-practice/31-dom-practice/script.js
@@ -2,9 +2,11 @@ const text = document.querySelector(".text");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", function(){
-    text.textContent = "반가워요!";
+    const language = ["반가워요", "사랑해요", "좋아해요", "신나요", "오늘도 잘되는날"];
     const rainbow = ["red", "orange", "yellow", "green", "blue", "navy", "purple", "pink"];
     const num = Math.floor(Math.random() * rainbow.length);
+    const seqs = Math.floor(Math.random() * language.length);
     text.style.color= rainbow[num];
+    text.textContent = language[seqs];
 
 });
